d9f88c22b4bed5aab558ab9c1133f610e3827890
Антон Кустов <a.kustov@zt.ru>
2026-03-22 18:57:49 +0300

    httpx examples

---
+++ httpx_authentication.py
@@ -0,0 +1,33 @@
+import httpx  # Импортируем библиотеку HTTPX
+
+# Данные для входа в систему
+login_payload = {
+    "email": "user@example.com",
+    "password": "string"
+}
+
+# Выполняем запрос на аутентификацию
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+# Выводим полученные токены
+print("Login response:", login_response_data)
+print("Status Code:", login_response.status_code)
+
+me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers={"Authorization": f"Bearer {login_response_data['token']['accessToken']}"})
+print("me response:", me_response.json())
+
+
+# Формируем payload для обновления токена
+refresh_payload = {
+    "refreshToken": login_response_data["token"]["refreshToken"]
+}
+
+# Выполняем запрос на обновление токена
+refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json=refresh_payload)
+refresh_response_data = refresh_response.json()
+
+
+# Выводим обновленные токены
+print("Refresh response:", refresh_response_data)
+print("Status Code:", refresh_response.status_code)
